9478fa210d64f2926e92463609ff7a999628e89d
mehtapgundogan <mehtapgundogan@gmail.com>
2013-05-01 13:42:10 +0300

    kullanıcının kendi yerelindeki depolardan istediğini uygulamaya ekleyebileceği arayüz oluşturuldu

--- github4linux/linux_github_gui.py
+++ github4linux/linux_github_gui.py
@@ -1,30 +1,108 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'untitled.ui'
-#
-# Created: Fri Apr 19 13:00:19 2013
-#      by: PyQt4 UI code generator 4.9.1
+# Form implementation generated from reading ui file 'repos.ui'
+# Created: Wed Mar 13 13:02:39 2013
+#      bmmplementation generated from reading ui file 'repos.ui': PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+import commands
 import sys
-from github import *
-from StringIO import StringIO
-import Image
-import urllib2
-from PIL import Image
-import requests
-from StringIO import StringIO
-import urllib, cStringIO
+import glob
+#from gui import *
 from urllib import *
 import json
+import requests
+from github import *
+
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
     _fromUtf8 = lambda s: s
 
+class FindLocalRepoWindow(QtGui.QMainWindow):
+    def __init__(self):
+	QtGui.QMainWindow.__init__(self)
+	self.setObjectName(_fromUtf8("Adding Local Repos"))
+        self.resize(800,600)
+        self.centralwidget = QtGui.QWidget(self)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+	self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 70, 251, 31))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(510, 40, 241, 211))
+        self.label_2.setText(_fromUtf8(""))
+        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("github-logo.png")))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 470, 111, 27))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(450, 470, 98, 27))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.listWidget = QtGui.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(50, 150, 401, 151))
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(self)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.setStatusBar(self.statusbar)
+	
+
+	# program kuruldugunda, kullanicinin yerelinde otomatik olarak bir dizin olusturuluyor
+
+        home = commands.getoutput('~/') # kullanicinin home dizinini buluruz.
+        home_name = home.split(" ")
+        home_name = home_name[2].split(":")
+        home = home_name[0]
+
+	print home
+
+        direc = commands.getstatusoutput('mkdir '+home+'/GitHubLINUX') # GitHubLINUX isminde bir dizin olusur arka planda. eger bu dizin daha onceden varsa ayni dizinden bir daha olusturulmayacaktir.
+
+
+        # .git uzantili dosyalari bulma islemi burada yapiliyor.
+        # Eger dosya bulunuyorsa deponun ismini diziden cekip arayuzde gosteriyoruz.
+
+
+	repos_name_array = []
+	array = []
+
+	for files in glob.iglob(home+"*/.git"):
+        	repos_name = files.split('/')
+        	repos_name_array.append(repos_name)
+
+	array_size = len(repos_name_array)
+	for i in range (0,array_size):
+        	array = repos_name_array[i]
+		self.listWidget.addItem(array[3])
+
+
+        self.retranslateUi(self)
+	QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.OpenUserPageWindow)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.close)
+        QtCore.QMetaObject.connectSlotsByName(self)
+    def OpenUserPageWindow(self):
+	
+
+        self.new = UserPageWindow()
+	self.close()
+	self.new.show()
+        
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">Find Local Repositories</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+                                                           
+	self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "ADD SELECTED", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "SKIP", None, QtGui.QApplication.UnicodeUTF8))
 class github:
 	def __init__(self, ui):
                 self.ui = ui
@@ -34,9 +112,10 @@ class github:
                 user = gh.users(edit_name).get()
                 return user
 
-class Ui_MainWindow(QtGui.QMainWindow):
+class UserPageWindow(QtGui.QMainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+	super(UserPageWindow,self).__init__()
+        #QtGui.QMainWindow.__init__(self)
         self.setObjectName(_fromUtf8("Github4Linux"))
         self.resize(800,600)
         self.centralwidget = QtGui.QWidget(self)
@@ -320,10 +399,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         #self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("MainWindow", "Tab 1", None, QtGui.QApplication.UnicodeUTF8))
 
-
 app = QtGui.QApplication(sys.argv)
-ui = Ui_MainWindow()
-ui.show()
+window = FindLocalRepoWindow()
+window.show()
 sys.exit(app.exec_())
-
-

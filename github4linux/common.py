ea41790348ada7b5ae882b65b60732a64951224b
kancer ezeroglu <ezeroglukancer@gmail.com>
2013-06-08 20:23:49 +0300

    degişken isimleri değiştirildi.

--- github4linux/common.py
+++ github4linux/common.py
@@ -1,4 +1,4 @@
 """ Common constants """
-CONSUMER_KEY = '0fa36f5e0e4a9e8bc5d6'
-CONSUMER_SECRET = '1362038cf19b885487539929003d9ed062550376'
-HOST = 'github4linux.com'
+consumer_key = '0fa36f5e0e4a9e8bc5d6'
+consumer_secret = '1362038cf19b885487539929003d9ed062550376'
+host = 'github4linux.com'

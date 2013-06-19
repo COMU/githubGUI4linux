529449fd75750cfacbea0d446b6199868a8b6209
mehtapgundogan <mehtapgundogan@gmail.com>
2013-06-19 10:50:34 +0300

    tüm arayüz dosyaları birleştirildi

--- github4linux/anapencere.py
+++ github4linux/anapencere.py
@@ -5,6 +5,7 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from linux_github_gui import FindLocalRepoWindow
 from githubApplication  import UserPageWindow as DigerPencere
+from oauth_authentication import Ui_MainWindow
 
 import sys
 
@@ -13,9 +14,9 @@ class DigerPencerem(QtGui.QDialog, DigerPencere):
         def __init__(self):
                 QtGui.QDialog.__init__(self)
                 self.setupUi(self)
-class MainWindow(QtGui.QMainWindow, FindLocalRepoWindow):
+class Pencere(QtGui.QDialog, FindLocalRepoWindow):
         def __init__(self):
-                QtGui.QMainWindow.__init__(self)
+                QtGui.QDialog.__init__(self)
 		self.setupUi(self)
                 self.digerPencere = DigerPencerem()
 		self.connect(self.listWidget, QtCore.SIGNAL("itemSelectionChanged()"), self.BackText)        	   	     	
@@ -26,19 +27,20 @@ class MainWindow(QtGui.QMainWindow, FindLocalRepoWindow):
 	def BackText(self):
 		print self.listWidget.currentItem().text()
         	self.digerPencere.localRepoList.append(self.listWidget.currentItem().text())
-		print self.digerPencere.localRepoList
-	        #self.digerPencere.repolistBox = QtGui.QTreeWidget(Dialog)
-	        #self.digerPencere.repolistBox.setGeometry(QtCore.QRect(10,30,150,200))
-        	#self.digerPencere.repolistBox.setHeaderLabels(["Repositorylist"])
-        	#root = QtGui.QTreeWidgetItem(self.digerPencere.repolistBox, ["Repositories"])
-        	#print self.localRepoList
 		root = QtGui.QTreeWidgetItem(self.digerPencere.repolistBox, ["Repositories"]) 
         	for i in self.digerPencere.localRepoList:
             		repo = QtGui.QTreeWidgetItem(root,[i]) 
-        	#print "listtttttttttt"
         	print self.digerPencere.localRepoList
-        	#self.digerPencere.repolistBox.show()
- 
+class MainWindow(QtGui.QMainWindow,Ui_MainWindow):
+	def __init__(self):
+                QtGui.QMainWindow.__init__(self)
+                self.setupUi(self)
+		self.pencere = Pencere()
+	@QtCore.pyqtSignature("bool")
+        def on_pushButton_clicked(self):
+                self.close()
+                self.pencere.exec_()
+	 
 app = QtGui.QApplication(sys.argv)
 window = MainWindow()
 window.show()
